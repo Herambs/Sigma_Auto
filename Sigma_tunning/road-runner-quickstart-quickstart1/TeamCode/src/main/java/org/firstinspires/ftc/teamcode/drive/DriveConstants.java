a890404fc03c71da7f81e0e8a4ffe0439afe6576
Heramb Sawant <herambs15@gmail.com>
2023-12-29 10:41:49 +0530

    Tunning until manual feedforward tunner

--- Sigma_tunning/road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ Sigma_tunning/road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01428 ;
-    public static double kA = 0.001;
-    public static double kStatic = 0.06;
+    public static double kV = 0.0135;
+    public static double kA = 0.0017;
+    public static double kStatic = 0.06841;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
